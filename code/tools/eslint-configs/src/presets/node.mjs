e798ebd611127de4901ae56b17058d259fdd9da5
Marc Fernandez <mcfdez87@gmail.com>
2026-05-02 09:08:42 +0200

    chore: add ESLint basics configuration

--- code/tools/eslint-configs/src/presets/node.mjs
+++ code/tools/eslint-configs/src/presets/node.mjs
@@ -1,11 +1,13 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import basePreset from './base.mjs';
+import basics from '../configs/basics.mjs';
 import { defineConfig } from 'eslint/config';
 
 /**
  * Node.js ESLint preset
  */
 export default defineConfig(
-    ...basePreset
+    ...basePreset,
+    ...basics
 );
