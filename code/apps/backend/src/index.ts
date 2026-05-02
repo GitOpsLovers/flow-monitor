172428665513a9a88b234cbe084ca091e8bc008e
Marc Fernandez <mcfdez87@gmail.com>
2026-05-02 10:13:49 +0200

    feat: load env files

--- code/apps/backend/src/index.ts
+++ code/apps/backend/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { webhooksRouter } from '@features/webhooks/ui/routes/webhooks.routes';
 
 const app = express()
-const port = 3000
+const port = process.env['PORT'] ?? 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
 
 app.use('/webhooks', webhooksRouter)
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Backend listening on port ${port}`)
+})
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        process.exit(0)
+    })
 })
