31dfaf71e3a2c5d56f83ef73d35434fd39eafaa7
Marc Fernandez <mcfdez87@gmail.com>
2026-05-04 10:05:10 +0200

    feat: add documentation basic pages

--- code/apps/docs/docusaurus.config.ts
+++ code/apps/docs/docusaurus.config.ts
@@ -2,39 +2,22 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
-  title: 'My Site',
+  title: 'Flow Monitor',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
-
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://flow-monitor.dev',
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'GitOpsLovers',
+  projectName: 'flow-monitor',
   onBrokenLinks: 'throw',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
   presets: [
     [
       'classic',
@@ -51,9 +34,7 @@ const config: Config = {
       } satisfies Preset.Options,
     ],
   ],
-
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
@@ -73,7 +54,7 @@ const config: Config = {
         },
 
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/GitOpsLovers/flow-monitor',
           label: 'GitHub',
           position: 'right',
         },
@@ -117,12 +98,12 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/GitOpsLovers/flow-monitor',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `MIT License | ${new Date().getFullYear()}`,
     },
     prism: {
       theme: prismThemes.github,
